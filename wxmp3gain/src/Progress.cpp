a4d0e00faac69195ace2be63f27c12d2105cf97d
Cristiano Nunes <cfgnunes@gmail.com>
2013-04-27 15:24:32 +0000

    Add locale support

--- wxmp3gain/src/Progress.cpp
+++ wxmp3gain/src/Progress.cpp
@@ -29,7 +29,7 @@ void Progress::Execute()
     int maxValue = listFiles->GetItemCount();
     bool cont = true;
 
-    wxProgressDialog dialog(_("Working progress"), _("Wait..."), maxValue, parent, wxPD_CAN_ABORT | wxPD_APP_MODAL | wxPD_ELAPSED_TIME | wxPD_ESTIMATED_TIME | wxPD_REMAINING_TIME);
+    wxProgressDialog dialog(_("Progress"), _("Wait..."), maxValue, parent, wxPD_CAN_ABORT | wxPD_APP_MODAL | wxPD_ELAPSED_TIME | wxPD_ESTIMATED_TIME | wxPD_REMAINING_TIME);
     dialog.Update(0, wxString::Format(_("Processed %i files of %i."), 0, maxValue));
     for (int i = 0; i < maxValue; i++)
     {
