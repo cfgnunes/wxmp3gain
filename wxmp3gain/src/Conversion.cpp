77ca1a8cd1253e09877b5d41de499a3be165c65d
Cristiano Nunes <cfgnunes@gmail.com>
2013-08-02 18:21:14 +0000

    Translation files updated

--- wxmp3gain/src/Conversion.cpp
+++ wxmp3gain/src/Conversion.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of the wxMP3gain and licensed under the GNU General Public License, version 3
- * http://www.gnu.org/licenses/gpl-3.0.html
+ * http://www.gnu.org/licenses/gpl-3.0.htmlc
  */
 
 #include "Conversion.h"
