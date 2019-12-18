ed60813bd6b638ab55f830f5cc7f3d7ecc254eb2
Cristiano Nunes <cfgnunes@gmail.com>
2019-12-18 03:31:13 -0300

    Updating gui project with wxFormBuilder 3.9.0

--- src/Gui.h
+++ src/Gui.h
@@ -1,12 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Feb 16 2016)
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __GUI_H__
-#define __GUI_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
@@ -21,11 +20,11 @@
 #include <wx/sizer.h>
 #include <wx/listctrl.h>
 #include <wx/gauge.h>
-#include <wx/button.h>
-#include <wx/panel.h>
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #include <wx/icon.h>
+#include <wx/button.h>
+#include <wx/panel.h>
 #include <wx/menu.h>
 #include <wx/statusbr.h>
 #include <wx/toolbar.h>
@@ -58,19 +57,15 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// Class Main
 ///////////////////////////////////////////////////////////////////////////////
-class Main : public wxFrame 
+class Main : public wxFrame
 {
 	private:
-	
+
 	protected:
 		wxBoxSizer* g_boxMain;
-		wxPanel* m_panel4;
 		wxBoxSizer* g_boxNormalVolume;
-		wxStaticText* m_staticText9;
 		wxTextCtrl* g_txtNormalVolume;
-		wxStaticText* m_staticText10;
 		wxBoxSizer* g_boxConstantGain;
-		wxStaticText* m_staticText11;
 		wxStaticText* g_lblConstantGain;
 		wxListCtrl* g_lstFiles;
 		wxGauge* g_gugProgress;
@@ -83,17 +78,17 @@ class Main : public wxFrame
 		wxMenu* g_mnbHelp;
 		wxStatusBar* g_mainStatusBar;
 		wxToolBar* g_mainToolBar;
-		wxToolBarToolBase* g_tbAddFolder; 
-		wxToolBarToolBase* g_tbAddFiles; 
-		wxToolBarToolBase* g_tbRemoveFiles; 
-		wxToolBarToolBase* g_tbClearList; 
-		wxToolBarToolBase* g_tbAnalyze; 
-		wxToolBarToolBase* g_tbGain; 
-		wxToolBarToolBase* g_tbSettings; 
-		wxToolBarToolBase* g_tbAbout; 
+		wxToolBarToolBase* g_tbAddFolder;
+		wxToolBarToolBase* g_tbAddFiles;
+		wxToolBarToolBase* g_tbRemoveFiles;
+		wxToolBarToolBase* g_tbClearList;
+		wxToolBarToolBase* g_tbAnalyze;
+		wxToolBarToolBase* g_tbGain;
+		wxToolBarToolBase* g_tbSettings;
+		wxToolBarToolBase* g_tbAbout;
 		wxMenu* g_mainMenu;
 		wxTimer m_timer1;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OntxtNormalVolumeTextKillFocus( wxFocusEvent& event ) { event.Skip(); }
 		virtual void OnlstFilesDeleteItem( wxListEvent& event ) { event.Skip(); }
@@ -117,41 +112,36 @@ class Main : public wxFrame
 		virtual void mnuWebsite( wxCommandEvent& event ) { event.Skip(); }
 		virtual void mnuAbout( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnTimer1Trigger( wxTimerEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
+
 		Main( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 790,450 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
-		
+
 		~Main();
-		
+
 		void MainOnContextMenu( wxMouseEvent &event )
 		{
 			this->PopupMenu( g_mainMenu, event.GetPosition() );
 		}
-	
+
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class Settings
 ///////////////////////////////////////////////////////////////////////////////
-class Settings : public wxDialog 
+class Settings : public wxDialog
 {
 	private:
-	
+
 	protected:
-		wxNotebook* m_notebook1;
-		wxPanel* m_panel1;
 		wxCheckBox* g_chkAutoLower;
 		wxCheckBox* g_chkForceInput;
-		wxPanel* m_panel2;
 		wxRadioButton* g_optTagAPE;
 		wxRadioButton* g_optTagID3;
 		wxRadioButton* g_optTagSKIP;
 		wxCheckBox* g_chkTagFORCE;
-		wxPanel* m_panel3;
 		wxCheckBox* g_chkConstantGain;
-		wxStaticText* m_staticText13;
 		wxSlider* g_sldConstantGain;
 		wxStaticText* g_lblConstantGain;
 		wxRadioButton* g_optBothChannel;
@@ -160,20 +150,19 @@ class Settings : public wxDialog
 		wxButton* g_btnDefault;
 		wxButton* g_btnOK;
 		wxButton* g_btnCancel;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void updateDisabledControlsEvent( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnsldConstantGainCmdSliderUpdated( wxScrollEvent& event ) { event.Skip(); }
 		virtual void OnbtnDefaultClick( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnbtnOKClick( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnbtnCancelClick( wxCommandEvent& event ) { event.Skip(); }
-		
-	
+
+
 	public:
-		
-		Settings( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Settings"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_DIALOG_STYLE ); 
+
+		Settings( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Settings"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_DIALOG_STYLE );
 		~Settings();
-	
+
 };
 
-#endif //__GUI_H__
