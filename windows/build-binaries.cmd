90569c9fe1afa9eefa0ffc217ae79c4b85c198d2
Cristiano Nunes <cfgnunes@gmail.com>
2019-12-20 02:41:24 -0300

    Update build-binaries.cmd

--- windows/build-binaries.cmd
+++ windows/build-binaries.cmd
@@ -50,7 +50,7 @@ echo Done!
 echo.
 
 echo Compiling translation files...
-for %%L in (cs,de,es,hr_HR,pt_BR,ru,tr) do (
+for %%L in (cs,de,es,hr_HR,ja,pt_BR,ru,tr) do (
 	mkdir build\resource\msg\%%L\
 	msgfmt ..\msg\%%L.po -o build\resource\msg\%%L\%APP_NAME%.mo
 	)
