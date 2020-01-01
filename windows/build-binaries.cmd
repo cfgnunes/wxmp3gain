4050d50133e07848128a697f8a22ad059dc75990
Cristiano Nunes <cfgnunes@gmail.com>
2020-01-01 20:21:59 -0300

    Update build-binaries.cmd

--- windows/build-binaries.cmd
+++ windows/build-binaries.cmd
@@ -1,9 +1,9 @@
 @echo off
 cls
 
-set APP_NAME=wxmp3gain
+set APP_NAME=wxmp3val
 set OUTPUT_DIR=build
-set PATH=%PATH%;C:\TDM-GCC-32\bin
+set PATH=%PATH%;C:\mingw32\bin
 
 echo Starting %APP_NAME% build...
 if exist %OUTPUT_DIR% rmdir %OUTPUT_DIR% /s /q
@@ -18,28 +18,27 @@ echo Done!
 echo.
 
 echo Compiling project...
-mingw32-g++.exe -pipe -mthreads -D__GNUWIN32__ -D__WXMSW__ -DWXUSINGDLL -DwxUSE_UNICODE -Wno-attributes -ansi -Wall -O2 -IC:\TDM-GCC-32\include -IC:\TDM-GCC-32\lib\gcc510TDM_dll\mswu -c ..\src\*.cpp
+g++ -pipe -mthreads -D__GNUWIN32__ -D__WXMSW__ -DWXUSINGDLL -DwxUSE_UNICODE -Wno-attributes -ansi -Wall -O2 -IC:\mingw32\include -IC:\wxWidgets-3.1.3-headers\include -IC:\wxMSW-3.1.3_gcc810_Dev\lib\gcc810_dll\mswu -c ..\src\*.cpp
 echo Done!
 echo.
 
 echo Compiling resource file...
-windres.exe -IC:\TDM-GCC-32\include -IC:\TDM-GCC-32\lib\gcc510TDM_dll\mswu -J rc -O coff -i resource.rc -o resource.res
+windres -IC:\TDM-GCC-32\include -IC:\wxWidgets-3.1.3-headers\include -IC:\wxMSW-3.1.3_gcc810_Dev\lib\gcc810_dll\mswu -J rc -O coff -i resource.rc -o resource.res
 echo Done!
 echo.
 
 echo Linking objects...
-mingw32-g++.exe -LC:\TDM-GCC-32\lib\gcc510TDM_dll -o %OUTPUT_DIR%\%APP_NAME%.exe *.o resource.res -s -mthreads -lwxbase31u -lwxbase31u_net -lwxbase31u_xml -lwxexpat -lwxjpeg -lwxmsw31u_adv -lwxmsw31u_aui -lwxmsw31u_core -lwxmsw31u_gl -lwxmsw31u_html -lwxmsw31u_media -lwxmsw31u_propgrid -lwxmsw31u_ribbon -lwxmsw31u_richtext -lwxmsw31u_stc -lwxmsw31u_webview -lwxmsw31u_xrc -lwxpng -lwxregexu -lwxscintilla -lwxtiff -lwxzlib -mwindows
+g++ -LC:\wxMSW-3.1.3_gcc810_Dev\lib\gcc810_dll -o %OUTPUT_DIR%\%APP_NAME%.exe *.o resource.res -s -mthreads -lwxbase31u -lwxbase31u_net -lwxbase31u_xml -lwxexpat -lwxjpeg -lwxmsw31u_adv -lwxmsw31u_aui -lwxmsw31u_core -lwxmsw31u_gl -lwxmsw31u_html -lwxmsw31u_media -lwxmsw31u_propgrid -lwxmsw31u_ribbon -lwxmsw31u_richtext -lwxmsw31u_stc -lwxmsw31u_webview -lwxmsw31u_xrc -lwxpng -lwxregexu -lwxscintilla -lwxtiff -lwxzlib -mwindows
 echo Done!
 echo.
 
 echo Copying files to "%OUTPUT_DIR%" directory...
+copy /y "..\docs\CHANGELOG" "%OUTPUT_DIR%\Changelog.txt"
+copy /y "..\docs\COPYING" "%OUTPUT_DIR%\License.txt"
+copy /y "..\docs\README" "%OUTPUT_DIR%\Readme.txt"
+copy /y "C:\wxMSW-3.1.3_gcc810_Dev\lib\gcc810_dll\wxbase313u_gcc810.dll" "%OUTPUT_DIR%\"
+copy /y "C:\wxMSW-3.1.3_gcc810_Dev\lib\gcc810_dll\wxmsw313u_core_gcc810.dll" "%OUTPUT_DIR%\"
 xcopy /h /s /f "..\resource\*" "%OUTPUT_DIR%\resource\"
-copy /y "%APP_NAME%.exe" "%OUTPUT_DIR%\"
-copy /y "C:\TDM-GCC-32\lib\gcc510TDM_dll\wxbase312u_gcc510TDM.dll" "%OUTPUT_DIR%\"
-copy /y "C:\TDM-GCC-32\lib\gcc510TDM_dll\wxmsw312u_core_gcc510TDM.dll" "%OUTPUT_DIR%\"
-copy "..\docs\COPYING" "%OUTPUT_DIR%\License.txt"
-copy "..\docs\CHANGELOG" "%OUTPUT_DIR%\Changelog.txt"
-copy "..\docs\README" "%OUTPUT_DIR%\Readme.txt"
 echo Done!
 echo.
 
@@ -50,7 +49,7 @@ echo Done!
 echo.
 
 echo Compiling translation files...
-for %%L in (cs,de,es,hr_HR,ja,pt_BR,ru,tr) do (
+for %%L in (cs,de,es,pt_BR,ru) do (
 	mkdir build\resource\msg\%%L\
 	msgfmt ..\msg\%%L.po -o build\resource\msg\%%L\%APP_NAME%.mo
 	)
