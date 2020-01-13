28ed21f52a61eb80375055620a391fe2501e8bf5
Cristiano Nunes <cfgnunes@gmail.com>
2020-01-12 23:20:12 -0300

    Small refactor

--- src/GuiFrameMain.cpp
+++ src/GuiFrameMain.cpp
@@ -363,9 +363,7 @@ void GuiFrameMain::loadResources() {
     wxString resourceDir = GetResourceDir();
 
     // Window icon
-    wxIcon frameIcon;
-    frameIcon.CopyFromBitmap(wxBitmap(wxImage(resourceDir + _T("icon-frame.ico"))));
-    SetIcon(frameIcon);
+    SetIcon(wxIcon(resourceDir + _T("icon-app.ico")));
 
     // Toolbar bitmaps
     g_mainToolBar->SetToolNormalBitmap(ID_ADD_FILES, wxBitmap(wxImage(resourceDir + _T("toolbar/add-file.png"))));
